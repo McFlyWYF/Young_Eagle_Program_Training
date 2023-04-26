3b2bb5c45ebe291a8fcc5340bcab9d5169af41b7
McFly <840954467@qq.com>
2023-04-26 11:00:42 +0800

    singleton

---
+++ Singleton/Refrigerator.py
@@ -0,0 +1,23 @@
+def Singleton(cls):
+    _instance = {}
+
+    def _singleton(*args, **kwargs):
+        if cls not in _instance:
+            _instance[cls] = cls(*args, **kwargs)
+        return _instance[cls]
+
+    return _singleton
+
+
+@Singleton
+class A(object):
+    a = 1
+
+    def __init__(self, x=0):
+        self.x = x
+
+
+a1 = A(2)
+a2 = A(3)
+
+print(a1.x, a2.x)  # 2, 2
